bc17b78dc34716aaeaad12588cd2b2e7dfaff3f3
KristianColville1 <kristiancolville1@gmail.com>
2022-09-06 16:11:46 +0100

    Added toasts logic for messages

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
-# Create your views here.
 
 def view_bag(request):
     """ A view that renders the bag contents page """
@@ -10,6 +11,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -30,13 +32,14 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
     
-    
+
 def adjust_bag(request, item_id):
-    """ Adjust the quantity of the specified product to the specified amount """
+    """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
     size = None
@@ -49,7 +52,7 @@ def adjust_bag(request, item_id):
             bag[item_id]['items_by_size'][size] = quantity
         else:
             del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size'][size]:
+            if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
     else:
         if quantity > 0:
@@ -60,8 +63,9 @@ def adjust_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
+
 def remove_from_bag(request, item_id):
-    """ Adjust the quantity of the specified product to the specified amount """
+    """Remove the item from the shopping bag"""
 
     try:
         size = None
@@ -71,12 +75,13 @@ def remove_from_bag(request, item_id):
 
         if size:
             del bag[item_id]['items_by_size'][size]
-            
-            if not bag[item_id]['items_by_size'][size]:
+            if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
         else:
             bag.pop(item_id)
+
         request.session['bag'] = bag
         return HttpResponse(status=200)
+
     except Exception as e:
         return HttpResponse(status=500)
